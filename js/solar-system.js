5c39432d6b67e7e37b0d82e6f315feeb1fd53018
dcdholder <dcdholder@gmail.com>
2017-05-15 14:51:23 -0400

    Added Mercury, Venus and Mars.

--- js/solar-system.js
+++ js/solar-system.js
@@ -1,83 +1,144 @@
-var app = new PIXI.Application({width: 800, height: 600, antialias: true, resolution: 1});
+var planets = {
+  mercury: {
+    color: 0xFFFFFF,
+    radius: 2440,
+    orbitRadius: 5.79*Math.pow(10,7),
+    yearLength: 0.241,
+    moons: []
+  },
+
+  venus: {
+    color: 0xFFFF00,
+    radius: 6052,
+    orbitRadius: 1.08*Math.pow(10,8),
+    yearLength: 0.616,
+    moons: []
+  },
+
+  earth: {
+    color: 0x0055FF,
+    radius: 6371,
+    orbitRadius: 1.50*Math.pow(10,8),
+    yearLength: 1.0,
+    moons: {
+      moon: {
+        color: 0xD3D3D3,
+        radius: 1737,
+        orbitRadius: 385000,
+        yearLength: 0.0833
+      }
+    }
+  },
+
+  mars: {
+    color: 0xFF5555,
+    radius: 3390,
+    orbitRadius: 2.28*Math.pow(10,8),
+    yearLength: 1.88,
+    moons: {
+      phobos: {
+        color: 0xD3D3D3,
+        radius: 11.1,
+        orbitRadius: 6000,
+        yearLength: 0.000913
+      },
+      deimos: {
+        color: 0xD3D3D3,
+        radius: 6.2,
+        orbitRadius: 23460,
+        yearLength: 0.00342
+      }
+    }
+  }
+};
+
+//fixed position
+var sun = {
+  radius: 6.96*Math.pow(10,5),
+  color:  0xFF5500
+};
+
+var kmPerPixelScale = {
+  moonOrbit:    15000,
+  planetOrbit:  5*Math.pow(10,5),
+  moonRadius:   320,
+  planetRadius: 320,
+  sunRadius:    16000
+};
+
+var borderColor = {
+  sun: 0xFF0000,
+  planet: 0x00FF00,
+  moon: 0x0000FF
+};
+
+var borderThickness = {
+  sun:    4,
+  planet: 3,
+  moon:   2
+};
+
+var app = new PIXI.Application({width: 1200, height: 900, antialias: true, resolution: 1});
 
 document.body.appendChild(app.view);
 
-var moonOrbitDistance  = 50;
-var moonRadius         = 10;
-var moonRevolutionTime = 5;
-var moonRadians        = 0;
+//initialize initial orbit angle of each orbiting body to 0
+for (let planet in planets) {
+  planets[planet].radians = Math.random()*2.0*Math.PI;
+  for (let moon in planets[planet].moons) {
+    planets[planet].moons[moon].radians = Math.random()*2.0*Math.PI;
+  }
+}
 
-var earthOrbitDistance  = 150;
-var earthRadius         = 20;
-var earthRevolutionTime = 60;
-var earthRadians        = 0;
+//initialize the shapes representing the planets and moons
+for (let planet in planets) {
+  planets[planet].graphics = new PIXI.Graphics();
 
-var sunRadius = 40;
+  planets[planet].graphics.beginFill(planets[planet].color);
+  planets[planet].graphics.lineStyle(borderThickness.planet,borderColor.planet,1);
+  planets[planet].graphics.drawCircle(0,0,planets[planet].radius/kmPerPixelScale.planetRadius+1); //add 1 to lower-bound radius
+  planets[planet].graphics.endFill();
 
-var sunPosition = {
-  x: app.renderer.width/2,
-  y: app.renderer.height/2
-};
+  app.stage.addChild(planets[planet].graphics);
 
-var earthPosition = {
-  x: app.renderer.width/2+earthOrbitDistance,
-  y: app.renderer.height/2
-};
+  for (let moon in planets[planet].moons) {
+    planets[planet].moons[moon].graphics = new PIXI.Graphics();
 
-var moonPosition = {
-  x: earthPosition.x+moonOrbitDistance,
-  y: earthPosition.y
-};
+    planets[planet].moons[moon].graphics.beginFill(planets[planet].moons[moon].color);
+    planets[planet].moons[moon].graphics.lineStyle(borderThickness.moon,borderColor.moon,1);
+    planets[planet].moons[moon].graphics.drawCircle(0,0,planets[planet].moons[moon].radius/kmPerPixelScale.moonRadius+1); //add 1 to lower-bound radius
+    planets[planet].moons[moon].graphics.endFill();
 
-//moon
-var moon = new PIXI.Graphics();
+    app.stage.addChild(planets[planet].moons[moon].graphics);
+  }
+}
 
-moon.beginFill(0xAAAAAA);
-moon.lineStyle(3,0xFF0000,1);
-moon.drawCircle(0,0,moonRadius);
-moon.endFill();
+//initialize the shape and set a fixed position for the sun
+sun.position = {};
+sun.position.x = app.renderer.width/2;
+sun.position.y = app.renderer.height/2;
 
-app.stage.addChild(moon);
+sun.graphics = new PIXI.Graphics();
 
-//earth
-var earth = new PIXI.Graphics();
+sun.graphics.beginFill(sun.color);
+sun.graphics.lineStyle(borderThickness.sun,borderColor.sun,1);
+sun.graphics.drawCircle(sun.position.x,sun.position.y,sun.radius/kmPerPixelScale.sunRadius);
+sun.graphics.endFill();
 
-earth.beginFill(0x00FF00);
-earth.lineStyle(3,0xFF00000,1);
-earth.drawCircle(0,0,earthRadius);
-earth.endFill();
-
-app.stage.addChild(earth);
-
-//sun
-var sun = new PIXI.Graphics();
-
-sun.beginFill(0xFF5500);
-sun.lineStyle(3,0xFF00000,1);
-sun.drawCircle(sunPosition.x,sunPosition.y,sunRadius);
-sun.endFill();
-
-app.stage.addChild(sun);
+app.stage.addChild(sun.graphics);
 
 app.ticker.add(function(delta) {
-  var trueTime = delta / 60;
+  var trueTime = delta / 60 / 60; //1 year = 1 minute
 
-  earthRadians += (1/earthRevolutionTime)*trueTime*2*Math.PI;
-  moonRadians  += (1/moonRevolutionTime)*trueTime*2*Math.PI;
+  for (let planet in planets) {
+    planets[planet].radians += (1/planets[planet].yearLength)*trueTime*2*Math.PI;
+    planets[planet].graphics.x = sun.position.x + (sun.radius/kmPerPixelScale.sunRadius + planets[planet].orbitRadius/kmPerPixelScale.planetOrbit)*Math.cos(planets[planet].radians);
+    planets[planet].graphics.y = sun.position.y - (sun.radius/kmPerPixelScale.sunRadius + planets[planet].orbitRadius/kmPerPixelScale.planetOrbit)*Math.sin(planets[planet].radians);
 
-  earthPosition = {
-    x: sunPosition.x + earthOrbitDistance*Math.cos(earthRadians),
-    y: sunPosition.y - earthOrbitDistance*Math.sin(earthRadians)
-  };
-
-  moonPosition = {
-    x: earthPosition.x + moonOrbitDistance*Math.cos(moonRadians),
-    y: earthPosition.y - moonOrbitDistance*Math.sin(moonRadians)
-  };
-
-  moon.x = moonPosition.x;
-  moon.y = moonPosition.y;
-
-  earth.x = earthPosition.x;
-  earth.y = earthPosition.y;
+    for (let moon in planets[planet].moons) {
+      planets[planet].moons[moon].radians += (1/planets[planet].moons[moon].yearLength)*trueTime*2*Math.PI;
+      planets[planet].moons[moon].graphics.x = planets[planet].graphics.x + (borderThickness.planet + borderThickness.moon + planets[planet].radius/kmPerPixelScale.planetRadius + planets[planet].moons[moon].orbitRadius/kmPerPixelScale.moonOrbit)*Math.cos(planets[planet].moons[moon].radians);
+      planets[planet].moons[moon].graphics.y = planets[planet].graphics.y - (borderThickness.planet + borderThickness.moon + planets[planet].radius/kmPerPixelScale.planetRadius + planets[planet].moons[moon].orbitRadius/kmPerPixelScale.moonOrbit)*Math.sin(planets[planet].moons[moon].radians);
+    }
+  }
 });
